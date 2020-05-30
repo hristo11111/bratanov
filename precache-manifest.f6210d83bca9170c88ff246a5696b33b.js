56d58bc779c9a87c92f6378692be5e6b04e97689
bratanov-fourth <hristo.bratanov@fourth.com>
2020-05-30 15:44:18 +0300

    Updates

---
+++ precache-manifest.f6210d83bca9170c88ff246a5696b33b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124bdea7e6522ba62b2e5ed4abd67f94",
+    "url": "/bratanov/index.html"
+  },
+  {
+    "revision": "a1c00ea0acaf1112fb3b",
+    "url": "/bratanov/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cdbe7bfcaca238e1ef2c",
+    "url": "/bratanov/static/js/2.66ad03fa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bratanov/static/js/2.66ad03fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1c00ea0acaf1112fb3b",
+    "url": "/bratanov/static/js/main.4f6c879a.chunk.js"
+  },
+  {
+    "revision": "cd5b12d77bc04c7e6d6a",
+    "url": "/bratanov/static/js/runtime-main.0b3bdede.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bratanov/static/media/logo.5d5d9eef.svg"
+  }
+]);
